9c44bbdbf84ef7c4c1f5fbbd95816a960b858fd8
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-17 08:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function theBeatlesPlay(musicians, instruments){
  let playInstrument = []
-   for(var i = 0; i < instruments.length; i++){
-     playInstrument.push(musicians[i] + "plays" +instrument[i])
+   for(var i = 0; i < musicians.length; i++){
+     playInstrument.push(musicians[i] + "plays" + instrument[i])
    }
    return playInstrument;
+}
+
+function johnLennonFacts(info){
+  let louder = [];
+  var i = 0;
+  while (i < info.length){
+    louder.push(info[i] + "!!!");
+    i++;
+  }
+  return louder;
+}
+
+function iLoveTheBeatles(num){
+  let beatles = [];
+  let i = 0;
+  do {
+    beatles.push("I love the Beatles!")
+    i++
+  } while (num < 15);
+  return beatles;
 }
